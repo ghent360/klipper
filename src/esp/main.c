e69696aaf7740bb7bd48e2e5aa79f68c3574157d
Venelin Efremov <ghent360@iqury.us>
2021-12-18 12:23:37 -0800

    Added empty ESP32 project.

---
+++ src/esp/main.c
@@ -0,0 +1,12 @@
+// Main starting point for ESP boards.
+//
+// Copyright (C) 2021  Ghent The Slicer <ghent360@iqury.us>
+//
+// This file may be distributed under the terms of the MIT license.
+
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}
