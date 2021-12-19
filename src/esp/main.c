f5c15519a23f60f542a7fe9c0b9cdc05dfcf4d62
Venelin Efremov <ghent360@iqury.us>
2021-12-19 01:15:38 -0800

    Add command to generate the compile_time_rule sources file.

--- src/esp/main.c
+++ src/esp/main.c
@@ -6,6 +6,8 @@
 
 #include <stdio.h>
 #include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "command.h" // DECL_CONSTANT
 #include "sched.h" // sched_main
 
@@ -13,5 +15,11 @@ DECL_CONSTANT_STR("MCU", CONFIG_IDF_TARGET);
 
 void app_main(void)
 {
+#if 0    
+    printf("Hello world\n");
+    while(1) {
+        vTaskDelay(1000);
+    }
+#endif    
     sched_main();
 }
--- src/esp/timer.c
+++ src/esp/timer.c
@@ -1,7 +1,58 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "esp/irq.h" // irq_disable
+#include "esp/misc.h" // timer_from_us
+#include "command.h" // DECL_CONSTANT
+#include "internal.h" // console_sleep
+#include "sched.h" // DECL_INIT
+
 uint32_t
 timer_read_time(void) {
     return xTaskGetTickCount();
+}
+
+void
+irq_disable(void)
+{
+}
+
+void
+irq_enable(void)
+{
+}
+
+irqstatus_t
+irq_save(void)
+{
+    return 0;
+}
+
+void
+irq_restore(irqstatus_t flag)
+{
+}
+
+void
+irq_wait(void)
+{
+#if 0    
+    // Must atomically sleep until signaled
+    if (!readl(&TimerInfo.must_wake_timers)) {
+        timer_disable_signals();
+        if (!TimerInfo.must_wake_timers)
+            console_sleep(&TimerInfo.ss_sleep);
+        timer_enable_signals();
+    }
+#endif
+    irq_poll();
+}
+
+void
+irq_poll(void)
+{
+#if 0    
+    if (readl(&TimerInfo.must_wake_timers))
+        timer_dispatch();
+#endif        
 }
