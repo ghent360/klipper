900288c67aa27e0c2e130435de4ac9b26ae0b263
Venelin Efremov <ghent360@iqury.us>
2021-12-21 14:32:22 -0800

    Adde more timer dummy functions.

--- src/esp/timer.c
+++ src/esp/timer.c
@@ -1,5 +1,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "autoconf.h"
 
 #include "esp/irq.h" // irq_disable
 #include "esp/misc.h" // timer_from_us
@@ -7,9 +8,45 @@
 #include "internal.h" // console_sleep
 #include "sched.h" // DECL_INIT
 
+DECL_CONSTANT("CLOCK_FREQ", CONFIG_CLOCK_FREQ);
+
+// Return true if time1 is before time2.  Always use this function to
+// compare times as regular C comparisons can fail if the counter
+// rolls over.
+uint8_t
+timer_is_before(uint32_t time1, uint32_t time2)
+{
+    return (int32_t)(time1 - time2) < 0;
+}
+
+// Activate timer dispatch as soon as possible
+void
+timer_kick(void)
+{
+}
+
+void
+timer_init(void)
+{
+}
+DECL_INIT(timer_init);
+
+void
+timer_reset(void)
+{
+}
+DECL_SHUTDOWN(timer_reset);
+
 uint32_t
 timer_read_time(void) {
     return xTaskGetTickCount();
+}
+
+// Return the number of clock ticks for a given number of microseconds
+uint32_t
+timer_from_us(uint32_t us)
+{
+    return us / 1000;
 }
 
 void
